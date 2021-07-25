4bd7b55121fc63101694c0b5c6ab419f31fdc6be
degenRobot <86513395+degenRobot@users.noreply.github.com>
2021-07-25 19:16:53 +1000

    added jupyter notebook with a few examples

--- model.py
+++ model.py
@@ -1,16 +1,17 @@
-
-
+import numpy
 
 def createLp(token1, token2, vol1, vol2) : 
     amm = {token1 : vol1, token2 : vol2}
     return amm
 
-def getPrices() : 
-    prices = {vaultToken : vol2/vol1, secondaryToken : vol1/vol2}
+def getPrices(token1, token2, vol1, vol2) : 
+    prices = {token1 : vol2/vol1, token2 : vol1/vol2}
     return prices
     
 
-def makeTrade(amount) : 
+def makeTrade(amount, vol1, vol2, fees = 0.003) : 
+    maxTrade = 0.3 #max amount of total volume that can be traded in single trade
+    amount = max(min(maxTrade*vol1, amount), -1*maxTrade*vol1)
     k = vol1 * vol2 
     vol1_new = vol1 + amount 
     vol2_new = k / (vol1 + amount)
@@ -27,39 +28,39 @@ def makeTrade(amount) :
 
         
     
-def createVault(vaultToken, amount, lendAllocation) : 
+def createVault(amount, lendAllocation, vaultToken, secondaryToken, vol1, vol2) : 
     collateral = lendAllocation * amount
     borrowAllocation = 1 - lendAllocation 
-    prices = getPrices()
+    prices = getPrices(vaultToken, secondaryToken, vol1, vol2)
     amountBorrowed = borrowAllocation*amount*prices[vaultToken]
     lpHolding = amountBorrowed / vol2
     return {'collateral' : collateral, 'debtAmount' : amountBorrowed, 'lpHolding' : lpHolding, 'pendingHarvest' : 0}
     
 
-def getVaultValue() : 
-    prices = getPrices()
-    balance = vault['collateral']  - vault['debtAmount'] / prices[vaultToken] + vault['lpHolding']*vol1*2
+def getVaultValue(vault, vaultToken, secondaryToken, vol1, vol2) : 
+    prices = getPrices(vaultToken, secondaryToken, vol1, vol2)
+    balance = vault['collateral']  - vault['debtAmount'] / prices[vaultToken] + vault['lpHolding']*vol1*2 + vault['pendingHarvest']
     return balance
 
 
-def adjVault(): 
+def adjVault(vault, adjRates, vol1): 
     vault['collateral'] += vault['collateral']*adjRates['lend']
     vault['debtAmount'] += vault['debtAmount']*adjRates['borrow']
     vault['pendingHarvest'] += (vault['lpHolding']*vol1*2)*adjRates['farm']
     
     
-def calcDebtRatio() : 
+def calcDebtRatio(vault, vaultToken, secondaryToken, vol1, vol2) : 
     debtRatio = vault['debtAmount'] / (vault['lpHolding']*vol2) 
     return debtRatio
 
-def calcCollatRatio() : 
-    prices = getPrices()
+def calcCollatRatio(vault, vaultToken, secondaryToken, vol1, vol2) : 
+    prices = getPrices(vaultToken, secondaryToken, vol1, vol2)
     collatRatio = (vault['debtAmount'] / prices[vaultToken]) / vault['collateral']
     return collatRatio
 
 
-def rebalanceDebt() : 
-    debtRatio = calcDebtRatio()
+def rebalanceDebt(vault, vaultToken, secondaryToken, vol1, vol2, debtLow, debtHigh, rebalanceAdj = .996) : 
+    debtRatio = calcDebtRatio(vault, vaultToken, secondaryToken, vol1, vol2)
     if debtRatio > debtHigh : 
         #too much debt ~ remove some LP
         lpRemoveAmt = (vault['debtAmount'] - vault['lpHolding']*vol2) #how much of secondary asset will be removed from LP
@@ -74,9 +75,9 @@ def rebalanceDebt() :
         vault['lpHolding'] += lpAdj
       
         
-def rebalanceCollat() : 
-    collatRatio = calcCollatRatio()
-    prices = getPrices()
+def rebalanceCollat(vault, vaultToken, secondaryToken, vol1, vol2, collatLow, collatTarget ,collatHigh) : 
+    collatRatio = calcCollatRatio(vault, vaultToken, secondaryToken, vol1, vol2) 
+    prices = getPrices(vaultToken, secondaryToken, vol1, vol2)
     if collatRatio < collatLow :
         adjAmt = (( vault['debtAmount']/prices[vaultToken]) - vault['collateral']*collatTarget) /  ( 1 + collatTarget)
         borrowAmt = adjAmt * prices[vaultToken]
@@ -93,62 +94,87 @@ def rebalanceCollat() :
         vault['collateral'] += adjAmt
         lpAdj = adjAmt / vol1 
         vault['lpHolding'] -= lpAdj
+        
+def harvest(vault, debtRatio, prices, vaultToken, harvestThreshold = 1, slippageAdj = .996) : 
+    if debtRatio < harvestThreshold : 
+        vault['collateral'] += vault['pendingHarvest']*(slippageAdj**2) #assume trade takes path through two LP's 
+    else : 
+        vault['debtAmount'] -= vault['pendingHarvest'] * prices[vaultToken] *(slippageAdj**2)
+        
+    vault['pendingHarvest'] = 0
+    
+    
+    
+       
+def simulate(allTrades, vaultInitial, lp, vaultToken, secondaryToken, vol1, vol2, adjRates, 
+             collatLow = .5, collatTarget = .55, collatHigh = .6, 
+             debtLow = .97, debtHigh = 1.03, rebalance = True, harvestFrequency = 5, ammFee = 0.003) : 
+    
+    vault = vaultInitial
+    priceLog = []
+    vaultLog = []
+    debtRatioLog = []
+    collatRatioLog = []
+    
+    
+    prices = getPrices(vaultToken, secondaryToken, vol1, vol2)
+    debtRatio = calcDebtRatio(vault, vaultToken, secondaryToken, vol1, vol2)
+    collatRatio = calcCollatRatio(vault, vaultToken, secondaryToken, vol1, vol2)   
+    vaultValue = getVaultValue(vault, vaultToken, secondaryToken, vol1, vol2)
+    vaultLog.append(vaultValue)
+    p0 = prices[secondaryToken]
+    priceLog.append(1.)
+    
+    
 
-def harvestTreshold        
-        
-        
-def simulate(allTrades, vault, lp, vaultToken) : 
+
+    
+    debtRatioLog.append(debtRatio)
+    collatRatioLog.append(collatRatio)
+    
+    harvestCount = 0
     
     nSteps = len(allTrades)
     for i in range(nSteps) : 
+
+        
         trades = allTrades[i]
-        for amt in trades : vol1, vol2 = makeTrade(amt)
-        adjVault()
-        rebalanceDebt()
-        rebalanceCollat()
+        for amt in trades : vol1, vol2 = makeTrade(amt,vol1, vol2)
+        
+        adjVault(vault, adjRates, vol1)
+        if rebalance == True : 
+            rebalanceDebt(vault, vaultToken, secondaryToken, vol1, vol2, debtLow, debtHigh)
+            rebalanceCollat(vault, vaultToken, secondaryToken, vol1, vol2, collatLow, collatTarget ,collatHigh)
+        
+        
+        prices = getPrices(vaultToken, secondaryToken, vol1, vol2)
+        debtRatio = calcDebtRatio(vault, vaultToken, secondaryToken, vol1, vol2)
+        collatRatio = calcCollatRatio(vault, vaultToken, secondaryToken, vol1, vol2) 
+        harvestCount += 1
+        if harvestCount == harvestFrequency : 
+            harvestCount = 0 
+            harvest(vault, debtRatio, prices, vaultToken)
+        
+        vaultValue = getVaultValue(vault, vaultToken, secondaryToken, vol1, vol2)
+        vaultLog.append(vaultValue)
+        priceLog.append(prices[secondaryToken] / p0)
+
+    
+        
+        debtRatioLog.append(debtRatio)
+        collatRatioLog.append(collatRatio)
+        
+    return vaultLog, priceLog, debtRatioLog, collatRatioLog
+        
  
-vaultToken = 'USDC'
-secondaryToken = 'FTM'
-vol1 =  10000
-vol2 =  50000
-fees = .003 # fee added to AMM on each exchange
 
-# VAULT INFO
-vaultTVL = 1000
-lendAllocation = .65
-debtLow = .97
-debtHigh = 1.03
-collatLow = .3
-collatTarget = .35
-collatHigh = .4
-harvestTreshold = 1
-rebalanceAdj = .996 #when rebalancing losses from swapping 
+#vault = createVault(vaultTVL, lendAllocation, vaultToken, secondaryToken, vol1, vol2)
 
-# APY's
-lendRate = .05
-borrowRate = .03
-farmRate = .45
 
-#SIM INFO
-stepsPerYear = 365
 
-annualRates = {'lend' : lendRate, 'borrow' : borrowRate, 'farm' : farmRate}
-adjRates = {'lend' : ((1 + lendRate)**(1/stepsPerYear) - 1), 
-            'borrow' : ((1 + borrowRate)**(1/stepsPerYear) - 1),
-            'farm' : ((1 + farmRate)**(1/stepsPerYear) - 1)}
+    
 
-lp = createLp(vaultToken, secondaryToken, vol1, vol2)
-vault = createVault(vaultToken, vaultTVL, lendAllocation)
-       
-nSteps = 100
-
-pBuy = .5
-lambdaTrades = 5 # input for poisson distribution
-expScale = 10 # input for exponential distribution to determine size of trade 
-
-import numpy
-
-def genTrades() : 
+def genTradesRandom(lambdaTrades, expScale, pBuy) : 
     nTrades = numpy.random.poisson(lambdaTrades)
     tradeSize = numpy.random.exponential(expScale, nTrades)
     rand = numpy.random.uniform(size = nTrades)
@@ -159,15 +185,5 @@ def genTrades() :
             amt = -1*amt
         trades.append(amt)
     return trades
-    
-allTrades = []
-        
-for i in range(nSteps) : 
-    tradeStep = genTrades()
-    allTrades.append(tradeStep)
-    
-print(getVaultValue())
-simulate(allTrades, vault, lp, vaultToken)
-print(getVaultValue())
 
 
